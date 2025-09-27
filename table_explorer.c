8aa700bd243aa55dc55d6c3feeb2a23b2e2ef27d
aghassanrr <167162651+AbdullahGhassanRR@users.noreply.github.com>
2025-09-27 20:03:28 +0700

    Update table_explorer.c

--- table_explorer.c
+++ table_explorer.c
@@ -14,7 +14,6 @@
 #define x 3940
 typedef struct table{
 	// change these member below according to your table column name 
-	// vvvvv
 	char loc[100];
 	char city[100];
 	long rprice;
@@ -464,5 +463,6 @@ int main()
 	
 	return 0;
 	}
+
 
 
